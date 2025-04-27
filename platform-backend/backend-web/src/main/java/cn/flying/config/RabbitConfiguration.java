e4087107a9a276acd17742400f0d9605b0597dea
flying <wbq1724593655@gmail.com>
2025-04-27 23:20:49 +0800

    Fix the issue of email sending failure

--- platform-backend/backend-web/src/main/java/cn/flying/config/RabbitConfiguration.java
+++ platform-backend/backend-web/src/main/java/cn/flying/config/RabbitConfiguration.java
@@ -2,6 +2,8 @@ package cn.flying.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,5 +20,14 @@ public class RabbitConfiguration {
         return QueueBuilder
                 .durable("mail")
                 .build();
+    }
+
+    /**
+     * 使用JSON消息转换器，避免Java序列化安全问题
+     * @return Jackson2JsonMessageConverter消息转换器
+     */
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
